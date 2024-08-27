4646ec020440395ff53f38227d64c37a7ce1da02
Giovanni Silva <git@gsilva.pro>
2024-08-26 21:40:22 -0300

    aula 06 - part 2 test unitario com exemplos de scenario

--- src/test/java/br/com/targettrust/CalculadoraIMCTest.java
+++ src/test/java/br/com/targettrust/CalculadoraIMCTest.java
@@ -1,6 +1,15 @@
 package br.com.targettrust;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,6 +28,33 @@ public class CalculadoraIMCTest {
         CalculadoraIMC calculadoraIMC = new CalculadoraIMC();
         var result = calculadoraIMC.calcularIMC(82.0f, 1.75f, 2);
         assertEquals(26.78f, result, 0.01);
+
+    }
+
+    /**
+     * Teste sera repetido com os valores de cada string separados por ,
+     */
+    @ParameterizedTest
+    @CsvSource({"82.0f,1.75f,26.78f","80.0f,1.79f,24.96f","75.0f,1.65f,27.54f"})
+    void calcularIMC_scenario_valores_corretos(float peso, float altura, double resultadoEsperado) {
+        CalculadoraIMC calculadoraIMC = new CalculadoraIMC();
+        var result = calculadoraIMC.calcularIMC(peso, altura, 2);
+        assertEquals(resultadoEsperado, result, 0.01);
+    }
+
+    @ParameterizedTest
+    @MethodSource("calculoIMCOutliner")
+    void calcularIMCOutliner(float peso, float altura, double resultadoEsperado) {
+        CalculadoraIMC calculadoraIMC = new CalculadoraIMC();
+        var result = calculadoraIMC.calcularIMC(peso, altura, 2);
+        assertEquals(resultadoEsperado, result, 0.01);
+    }
+
+    // Gerando aleatoriamente os resultados, não é previsiviel, ou seja o teste pode falhar aleatoriamente.
+    private static Stream<Arguments> calculoIMCOutliner() {
+        Random random = new Random();
+        return IntStream.range(1, 100)
+                .mapToObj(valor -> Arguments.of(random.nextFloat() * 1000, random.nextFloat()* 1000, 0.0d));
 
     }
 
